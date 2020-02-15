e7bb4142f34defe5e7238d969a45db65538d2f03
Amenemhat <AmenemhatD@gmail.com>
2020-02-15 17:24:15 +0200

    Code clean

--- index.js
+++ index.js
@@ -1,45 +1,35 @@
 require("dotenv").config();
 
 // Подключаем библиотеку для работы с Telegram API в переменную
-var TelegramBot = require("node-telegram-bot-api");
+let TelegramBot = require("node-telegram-bot-api");
 
 if (!process.env.TELEGRAM_TOKEN) {
   throw new Error("TELEGRAM_TOKEN env variable is missing");
 }
 
 // Включить опрос сервера. Бот должен обращаться к серверу Telegram, чтобы получать актуальную информацию
-// Подробнее: https://core.telegram.org/bots/api#getupdates
-var bot = new TelegramBot(process.env.TELEGRAM_TOKEN, { polling: true });
+let bot = new TelegramBot(process.env.TELEGRAM_TOKEN, { polling: true });
 
-//обработчик события polling_error
 bot.on("polling_error", m => console.log(m));
 
-// Написать мне ... (/echo Hello World! - пришлет сообщение с этим приветствием, то есть "Hello World!")
 bot.onText(/\/echo (.+)/, function(msg, match) {
-  var fromId = msg.from.id; // Получаем ID отправителя
-  var resp = match[1]; // Получаем текст после /echo
-  bot.sendMessage(fromId, resp);
-  console.log(resp);
+  let chatId = msg.chat.id;
+  let resp = match[1];
+  bot.sendMessage(chatId, resp);
 });
 
 bot.on("message", function(msg) {
-  console.log(msg);
   if (msg.text.toLowerCase().includes("привет")) {
-    var chatId = msg.chat.id; // Берем ID чата (не отправителя)
-    var message = "Привет!";
+    let chatId = msg.chat.id;
+    let message = "Привет!";
     bot.sendMessage(chatId, message);
-    console.log(message.toString());
   }
 });
 
-// Простая команда без параметров
 bot.on("message", function(msg) {
-  console.log(msg);
   if (msg.text.toLowerCase().includes("кот")) {
-    var chatId = msg.chat.id; // Берем ID чата (не отправителя)
-    // Фотография может быть: путь к файлу, поток (stream) или параметр file_id
-    var photo = "tap.jpg"; // в папке с ботом должен быть файл "cats.png"
+    let chatId = msg.chat.id;
+    let photo = "tap.jpg";
     bot.sendPhoto(chatId, photo, { caption: "Милые котята" });
-    console.log(photo);
   }
 });
