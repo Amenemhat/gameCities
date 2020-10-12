001499e69fa5733f5f7bd0923534a0a806606e90
Amenemhat <AmenemhatD@gmail.com>
2020-10-12 18:08:51 +0300

    Add knex query bilder, make some test query

--- src/db.js
+++ src/db.js
@@ -1,5 +1,109 @@
+//const { table } = require("console");
 const fs = require("fs");
 const progressFile = "./progress.json";
+
+const knex = require("knex")({
+  client: "pg",
+  connection: {
+    host: "localhost",
+    user: "postgres",
+    password: "123456",
+    database: "game_sities_db",
+  },
+});
+
+knex.schema.hasTable("users").then((exists) => {
+  if (!exists) {
+    return knex.schema
+      .createTable("users", (table) => {
+        table.increments("id").primary();
+        table.integer("chatID");
+        table.string("userName", 100);
+        table.integer("highScore");
+      })
+      .then((rows) => {
+        console.log(rows);
+      })
+      .catch((e) => {
+        console.error(e);
+      });
+  } else {
+    return knex
+      .select()
+      .from("users")
+      .then((rows) =>
+        rows.map((row) => {
+          console.log(row);
+        })
+      )
+      .catch((e) => {
+        console.error(e);
+      });
+  }
+});
+
+knex.schema.hasTable("sessions").then((exists) => {
+  if (!exists) {
+    return knex.schema
+      .createTable("sessions", (table) => {
+        table.increments("id").primary();
+        table.integer("chatID");
+        table.text("spentCities");
+        table.string("lastLetter", 1);
+        table.integer("scoreInSession");
+        table.boolean("open");
+      })
+      .then((rows) => {
+        console.log(rows);
+      })
+      .catch((e) => {
+        console.error(e);
+      });
+  } else {
+    return knex
+      .select()
+      .from("sessions")
+      .then((rows) =>
+      rows.map((row) => {
+        console.log(row);
+      })
+      )
+      .catch((e) => {
+        console.error(e);
+      });
+  }
+});
+
+knex.schema.hasColumn("sessions", "open").then((exist) => {
+  if (!exist) {
+    return knex.schema
+      .table("sessions", (table) => {
+        table.boolean("open");
+      })
+      .then(() =>
+        knex("sessions").insert({
+          chatID: 1234123,
+          spentCities: "Харьков, Воронеж, Житомир",
+          lastLetter: "р",
+          scoreInSession: 3,
+          open: true,
+        })
+      )
+      .then((rows) => {
+        console.log(rows);
+      })
+      .catch((e) => {
+        console.error(e);
+      });
+  }
+});
+
+// knex.schema
+//   .dropTableIfExists("users")
+//   .then((rows) => console.log(rows))
+//   .catch((e) => {
+//     console.error(e);
+//   });
 
 async function readProgressFromFile() {
   return new Promise((resolve, reject) => {
@@ -13,6 +117,26 @@ async function readProgressFromFile() {
         resolve(JSON.parse(data));
       }
     });
+  });
+}
+
+async function saveProgressToDB(botContext) {
+  knex.schema.hasTable("sessions").then((exists) => {
+    if (exists) {
+      return knex("sessions").insert({
+        chatID: botContext.chatID,
+        spentCities: botContext.sessions[botContext.chatID].spentCities.join(", "),
+        lastLetter: botContext.sessions[botContext.chatID].lastLetter,
+        scoreInSession: botContext.sessions[botContext.chatID].scoreInSession,
+        open: botContext.sessions[botContext.chatID].open,
+      })
+        .then((rows) => {
+          console.log(rows);
+        })
+        .catch((e) => {
+          console.error(e);
+        });
+    } 
   });
 }
 
@@ -31,6 +155,7 @@ async function makeSession(botContext) {
     spentCities: [],
     lastLetter: "",
     scoreInSession: 0,
+    open: true,
   };
   await saveProgressToFile(botContext);
 }
@@ -43,6 +168,7 @@ async function deleteSession(botContext) {
 module.exports = {
   readProgressFromFile,
   saveProgressToFile,
+  saveProgressToDB,
   makeSession,
   deleteSession,
 };
